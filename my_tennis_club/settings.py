d467a04fedf596782e5f045da10e6f00f5e318dc
yanshiii <144547830+yanshiii@users.noreply.github.com>
2025-04-20 12:35:43 +0530

    Update settings.py

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v9i37%()(4ezu137%wii2x%a+efp@g9ta^z_0$7nvw^8d*)798'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*.vercel.app']
 
 
 # Application definition
