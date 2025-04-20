814b01fdd1a9e25948a58fb7979661102315d7be
yanshiii <144547830+yanshiii@users.noreply.github.com>
2025-04-20 13:25:06 +0530

    Fix Vercel deployment issues

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -28,6 +28,7 @@ DEBUG = False
 
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
@@ -122,7 +123,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 
 STATICFILES_DIRS = [
