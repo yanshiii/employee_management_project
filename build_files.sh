f93dba40c7f385457a4492b6c3c620240deb760a
yanshiii <144547830+yanshiii@users.noreply.github.com>
2025-04-20 13:19:41 +0530

    Fix Vercel deployment issues

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
